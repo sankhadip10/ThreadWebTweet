2869179dc2ba6bbac91a8511bdb4b927b87f29bb
Sankhadip Das <ricky.das25@gmail.com>
2023-07-07 01:48:33 +0530

    Added html for tweeting

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Welcome to the homepage!'
+def index():
+    name = 'Sankhadip'
+    return render_template('index.html', name=name)
 
-@app.route('/about')
-def about():
-    return 'This is the about page.'
-
-@app.route('/contact')
-def contact():
-    return 'Contact us at example@example.com.'
 
 if __name__ == '__main__':
     app.run()
